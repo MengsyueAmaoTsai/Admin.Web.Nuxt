32c461e6a379beb854b42ca0c085668d8c9fa3d4
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-03-11 07:12:11 +0800

    enable server side render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: true,
   srcDir: './src',
   alias: {
     '@': resolve(__dirname, './src')
