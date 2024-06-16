8ae40395f26e0ce09c11d5f7c6199fe351863ac0
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-16 09:03:16 +0800

    add users page

--- src/pages/signal-sources/index.vue
+++ src/pages/signal-sources/index.vue
@@ -1,3 +1,40 @@
 <template>
-  <dv> Signal Sources </dv>
+  <h1>Signal Sources</h1>
+
+  <button>Create</button>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="source in signalSources" :key="source.id">
+        <td>{{ source.id }}</td>
+        <td>{{ source.name }}</td>
+        <td>
+          {{ source.description }}
+        </td>
+        <td>
+          <button>Details</button>
+          <button>Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script lang="ts" setup>
+const signalSources = [
+  { id: 1, name: "Source 1", description: "Description 1" },
+  { id: 2, name: "Source 2", description: "Description 2" },
+  { id: 3, name: "Source 3", description: "Description 3" },
+  { id: 4, name: "Source 4", description: "Description 4" },
+  { id: 5, name: "Source 5", description: "Description 5" },
+];
+</script>
--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,3 +1,37 @@
 <template>
-  <div>Users</div>
+  <h1>Users</h1>
+
+  <button>Create</button>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Id</th>
+        <th>Email</th>
+        <th>Name</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.id }}</td>
+        <td>{{ user.name }}</td>
+        <td>
+          {{ user.description }}
+        </td>
+        <td>
+          <button>Details</button>
+          <button>Delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
+
+<script lang="ts" setup>
+const users = [
+  { id: 1, email: "test@gmail.com", name: "User 1" },
+  { id: 2, email: "test2@gmail.com", name: "User 2" },
+];
+</script>
