ab3d27aa1600c2469e7e046bc288962096a17533
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 14:23:00 +0800

    list signal source and get details

---
+++ src/pages/signal-sources/[signalSourceId].vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    {{ source }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const id = route.params.signalSourceId;
+const url = `http://localhost:11000/api/v1/signal-sources/${id}`;
+const { data: source, error, execute, refresh } = await useFetch(url);
+console.log(source, error, execute, refresh);
+</script>
--- src/pages/signal-sources/index.vue
+++ src/pages/signal-sources/index.vue
@@ -14,15 +14,12 @@
     </thead>
 
     <tbody>
-      <tr v-for="source in signalSources" :key="source.id">
+      <tr v-for="source in signalSources.items" :key="source.id">
         <td>{{ source.id }}</td>
         <td>{{ source.name }}</td>
+        <td>{{ source.description }}</td>
         <td>
-          {{ source.description }}
-        </td>
-        <td>
-          <button>Details</button>
-          <button>Delete</button>
+          <NuxtLink :to="`/signal-sources/${source.id}`">Details</NuxtLink>
         </td>
       </tr>
     </tbody>
@@ -30,11 +27,10 @@
 </template>
 
 <script lang="ts" setup>
-const signalSources = [
-  { id: 1, name: "Source 1", description: "Description 1" },
-  { id: 2, name: "Source 2", description: "Description 2" },
-  { id: 3, name: "Source 3", description: "Description 3" },
-  { id: 4, name: "Source 4", description: "Description 4" },
-  { id: 5, name: "Source 5", description: "Description 5" },
-];
+const {
+  data: signalSources,
+  error,
+  execute,
+  refresh,
+} = await useFetch("http://localhost:11000/api/v1/signal-sources");
 </script>
