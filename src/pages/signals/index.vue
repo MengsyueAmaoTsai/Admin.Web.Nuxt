7577d4af01d24bc0b3fdae0f1790960328cbca53
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 14:27:58 +0800

    list signals

---
+++ src/pages/signals/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h3>
+    Total Signals: {{ signals.items.length }} Average Latency:
+    {{ averageLatency }}
+  </h3>
+  <div v-for="signal in signals.items" :key="signal.id">{{ signal }}</div>
+</template>
+
+<script lang="ts" setup>
+const {
+  data: signals,
+  error,
+  execute,
+  refresh,
+} = await useFetch("http://localhost:11000/api/v1/signals");
+</script>
