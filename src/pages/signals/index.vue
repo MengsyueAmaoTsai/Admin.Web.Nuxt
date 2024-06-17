d04c56b114860361184eee241822140929587e34
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-17 15:04:29 +0800

    calculate average latencies

--- src/pages/signals/index.vue
+++ src/pages/signals/index.vue
@@ -1,7 +1,7 @@
 <template>
   <h3>
     Total Signals: {{ signals.items.length }} Average Latency:
-    {{ averageLatency }}
+    {{ averageLatency }} seconds
   </h3>
   <div v-for="signal in signals.items" :key="signal.id">{{ signal }}</div>
 </template>
@@ -13,4 +13,16 @@ const {
   execute,
   refresh,
 } = await useFetch("http://localhost:11000/api/v1/signals");
+const averageLatency = computed(() => {
+  if (!signals.value || signals.value.items.length === 0) {
+    return (0).toFixed(3);
+  }
+  const totalLatency = signals.value.items.reduce(
+    (sum, signal) => sum + signal.latency,
+    0
+  );
+  const averageLatencyInMs = totalLatency / signals.value.items.length;
+  const averageLatencyInSeconds = averageLatencyInMs / 1000;
+  return averageLatencyInSeconds.toFixed(3);
+});
 </script>
