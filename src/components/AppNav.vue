fc6910a7fae55783c774cd811d50c427a7baff7b
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-18 03:39:59 +0800

    add page navigation

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <header>Hello AppNav</header>
+    <header>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/files">Files</NuxtLink>
+      <NuxtLink to="/users">Users</NuxtLink>
+      <NuxtLink to="/signal-sources">Signal Sources</NuxtLink>
+      <NuxtLink to="/signals">Signals</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+    </header>
   </div>
 </template>
