9b7bde52c7c966f2f917dc58bc6bad3ed95240dc
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-20 05:57:21 +0800

    edit user properties

---
+++ src/pages/users/[userId]/edit-properties.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Meng syue tsai</h2>
+    <p>Properties</p>
+  </div>
+
+  <div>
+    <button>Refresh</button>
+  </div>
+
+  <div>
+    <button>Save</button>
+    <button>Cancel</button>
+  </div>
+
+  <div>This view only contains properties that can be updated.</div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const userId = ref(route.params.userId);
+
+const {
+  data: user,
+  error,
+  execute,
+  refresh,
+} = await useFetch(`http://localhost:11000/api/v1/users/${userId.value}`);
+</script>
