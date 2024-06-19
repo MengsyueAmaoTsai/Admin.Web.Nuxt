fed5f42238049f6de5f5d6a424013e46c25a5836
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-19 12:10:03 +0800

    add settings page and audit logs page

---
+++ src/pages/audit-logs/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="root">
+    <h1>Audit Logs</h1>
+  </div>
+</template>
+<script lang="ts" setup></script>
---
+++ src/pages/settings/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div id="root">
+    <h1>Settings</h1>
+  </div>
+</template>
+<script lang="ts" setup></script>
