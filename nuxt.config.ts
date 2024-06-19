30dac18a84a96146fc718a3471f4cc0119c7dc10
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-19 18:22:46 +0800

    refactor about

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,10 @@ export default defineNuxtConfig({
 		plugins: "plugins",
 		modules: "modules",
 	},
+	appConfig: {
+		nuxt: {
+			appId: "RichillCapital.Admin.Web",
+			version: "1.0.0.0",
+		},
+	},
 });
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,10 +1,34 @@
+<script setup lang="ts">
+const appConfig = useAppConfig();
+const app = useNuxtApp();
+
+const assemblyName = appConfig.nuxt.appId;
+const assemblyVersion = appConfig.nuxt.version;
+const nuxtVersion = app.versions.nuxt;
+const vueVersion = app.versions.vue;
+</script>
+
 <template>
   <div>
-    <h1>Nuxt Version: {{ app.versions.nuxt }}</h1>
-    <h1>Vue Version: {{ app.versions.vue }}</h1>
+    <table>
+      <tbody>
+        <tr>
+          <th>Assembly Name</th>
+          <td>{{ assemblyName }}</td>
+        </tr>
+        <tr>
+          <th>Assembly Version</th>
+          <td>{{ assemblyVersion }}</td>
+        </tr>
+        <tr>
+          <th>Nuxt Version</th>
+          <td>{{ nuxtVersion }}</td>
+        </tr>
+        <tr>
+          <th>Vue Version</th>
+          <td>{{ vueVersion }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
-
-<script lang="ts" setup>
-const app = useNuxtApp();
-</script>
