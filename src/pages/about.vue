31c8a5e3770b793a1fed5886f566a858013b70bb
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-19 12:23:39 +0800

    display current nuxt and vue version

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Hello About</div>
+  <div>
+    <h1>Nuxt Version: {{ app.versions.nuxt }}</h1>
+    <h1>Vue Version: {{ app.versions.vue }}</h1>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts" setup>
+const app = useNuxtApp();
+</script>
