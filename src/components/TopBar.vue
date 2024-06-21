0915c039a52efada9b7329e364bcb012e4dfde01
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-21 21:08:23 +0800

    top bar

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,7 +1,55 @@
 <template>
-  <div>{{ useAppConfig().nuxt.appId }}</div>
+  <div class="root">
+    <div>
+      <fluent-button
+        @click="toggleDrawer"
+        appearance="outline"
+        class="hamburger-button"
+        >☰</fluent-button
+      >
+    </div>
+    <div>RichillCapital Admin</div>
+    <div>
+      <fluent-text-field
+        placeholder="Search resources, services, and docs (G+/)"
+      ></fluent-text-field>
+    </div>
+    <div>User Profile</div>
+  </div>
+
+  <div v-if="isDrawerOpen" class="side-menu">
+    <div class="menu-content">
+      <NuxtLink to="/users">Users</NuxtLink>
+      <NuxtLink to="/accounts">Accounts</NuxtLink>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const isDrawerOpen = ref(false);
 
-<style scoped></style>
+const toggleDrawer = (): void => {
+  isDrawerOpen.value = !isDrawerOpen.value;
+
+  console.log("Drawer is now", isDrawerOpen.value ? "open" : "closed");
+};
+</script>
+
+<style scoped>
+.root {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #0078d4;
+  color: white;
+}
+
+.hamburger-button {
+  font-size: 20px;
+  cursor: pointer;
+}
+
+.menu-content {
+  display: flex;
+}
+</style>
