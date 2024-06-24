680d8d1befe01f53cf2695501c8f7e6ed8a65475
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-24 15:05:29 +0800

    add scrumbreads

--- src/pages/signals/index.vue
+++ src/pages/signals/index.vue
@@ -1,33 +1,38 @@
 <template>
   <div>
-    <h2>Accounts</h2>
+    <fluent-breadcrumb>
+      <fluent-breadcrumb-item href="/">Home</fluent-breadcrumb-item>
+      <fluent-breadcrumb-item>Signals</fluent-breadcrumb-item>
+    </fluent-breadcrumb>
+
+    <h2>Signals</h2>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Source Id</th>
+          <th>Time</th>
+          <th>Exchange</th>
+          <th>Symbol</th>
+          <th>Quantity</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="signal in signals.items" :key="signal.id">
+          <td>{{ signal.id }}</td>
+          <td>{{ signal.sourceId }}</td>
+          <td>{{ signal.time }}</td>
+          <td>{{ signal.exchange }}</td>
+          <td>{{ signal.symbol }}</td>
+          <td>{{ signal.quantity }}</td>
+          <td>{{ signal.price }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
-
-  <table>
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Source Id</th>
-        <th>Time</th>
-        <th>Exchange</th>
-        <th>Symbol</th>
-        <th>Quantity</th>
-        <th>Price</th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr v-for="signal in signals.items" :key="account.id">
-        <td>{{ signal.id }}</td>
-        <td>{{ signal.sourceId }}</td>
-        <td>{{ signal.time }}</td>
-        <td>{{ signal.exchange }}</td>
-        <td>{{ signal.symbol }}</td>
-        <td>{{ signal.quantity }}</td>
-        <td>{{ signal.price }}</td>
-      </tr>
-    </tbody>
-  </table>
 </template>
 
 <script lang="ts" setup>
@@ -39,6 +44,8 @@ const {
   execute,
   refresh,
 } = await useFetch(`${resourceServerOptions.baseAddress}/api/v1/signals`);
+
+console.log(signals);
 </script>
 
 <style></style>
