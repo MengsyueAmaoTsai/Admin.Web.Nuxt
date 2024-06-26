301e60a82dd57209daae4e280be97a9dd4a4e95d
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-26 18:33:35 +0800

    Add top bar elementes

---
+++ src/components/AvatarMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button :title="title">
+      <div>
+        {{ name }}
+      </div>
+      <div>
+        <img src="" alt="avatar" />
+      </div>
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const name = "Mengsyue Tsai";
+const email = "mengsyue.tsai@outlook.com";
+
+const title = `Name: ${name}\nEmail: ${email}`;
+</script>
+
+<style></style>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="top-bar">
     <Brand />
+
+    <div>
+      <input
+        type="text"
+        placeholder="Search resources, services, and docs (G+/)"
+      />
+    </div>
+    <div class="top-bar-buttons">
+      <a title="Notifications">Notifications</a>
+      <a title="Settings">Settings</a>
+    </div>
+
+    <AvatarMenu />
   </div>
 </template>
 
