c9ef81d9dd56caacc34b20c3a3adab904bd20af6
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-06-27 01:58:04 +0800

    users page

--- src/pages/users/[userId]/index.vue
+++ src/pages/users/[userId]/index.vue
@@ -1,93 +1,13 @@
 <template>
-  <fluent-breadcrumb>
-    <fluent-breadcrumb-item href="/">Home</fluent-breadcrumb-item>
-    <fluent-breadcrumb-item href="/users">Users</fluent-breadcrumb-item>
-    <fluent-breadcrumb-item>{{ user.name }}</fluent-breadcrumb-item>
-  </fluent-breadcrumb>
-
   <div>
-    <h2>{{ user.name }}</h2>
-    <p>User</p>
+    
   </div>
-
-  <div>
-    <button>Edit properties</button>
-    <button>Delete</button>
-    <button>Refresh</button>
-    <button>Reset password</button>
-    <button>Revoke sessions</button>
-    <button>Manage View</button>
-  </div>
-
-  <h3>Basic Info</h3>
-  <table>
-    <tbody>
-      <tr>
-        <th>User Id</th>
-        <td>{{ user.id }}</td>
-      </tr>
-
-      <tr>
-        <th>Name</th>
-        <td>{{ user.name }}</td>
-      </tr>
-
-      <tr>
-        <th>Email</th>
-        <td>{{ user.email }}</td>
-      </tr>
-
-      <tr>
-        <th>Email Confirmed</th>
-        <td>{{ user.emailConfirmed }}</td>
-      </tr>
-
-      <tr>
-        <th>Lockout Enabled</th>
-        <td>{{ user.lockoutEnabled }}</td>
-      </tr>
-
-      <tr>
-        <th>Access Failed Count</th>
-        <td>{{ user.accessFailedCount }}</td>
-      </tr>
-      <tr>
-        <th>Created At</th>
-        <td>{{ user.createdAt }}</td>
-      </tr>
-    </tbody>
-  </table>
-
-  <h3>Accounts</h3>
-
-  <table>
-    <thead>
-      <tr>
-        <th>Id</th>
-        <th>Name</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="account in user.accounts">
-        <td>{{ account.id }}</td>
-        <td>{{ account.name }}</td>
-      </tr>
-    </tbody>
-  </table>
 </template>
 
-<script setup lang="ts">
-const route = useRoute();
-const userId = ref(route.params.userId);
+<script lang="ts" setup>
 
-const resourceServerOptions = useRuntimeConfig().public.resourceServer;
-
-const {
-  data: user,
-  error,
-  execute,
-  refresh,
-} = await useFetch(
-  `${resourceServerOptions.baseAddress}/api/v1/users/${userId.value}`
-);
 </script>
+
+<style>
+
+</style>
--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,78 +1,102 @@
 <template>
   <div>
-    <fluent-breadcrumb>
-      <fluent-breadcrumb-item href="/">Home</fluent-breadcrumb-item>
-      <fluent-breadcrumb-item>Users</fluent-breadcrumb-item>
-    </fluent-breadcrumb>
-
-    <h2>Users</h2>
-
-    <!-- Operations -->
-    <div>
-      <!-- <button @click="navigateToCreate">New User</button> -->
-      <NuxtLink to="/users/create"
-        ><fluent-button appearance="accent">New User</fluent-button></NuxtLink
-      >
-
-      <fluent-button appearance="accent">Download Users</fluent-button>
-
-      <!-- Bulk Operations -->
-      <select>
-        <option>Bulk Create</option>
-        <option>Bulk Delete</option>
-      </select>
-      <fluent-button appearance="accent">Refresh</fluent-button>
-
-      <!-- Manage View -->
-      <select>
-        <option>Bulk Create</option>
-        <option>Bulk Delete</option>
-      </select>
-
-      <fluent-button appearance="accent">Delete</fluent-button>
+    <div class="breadcrumb-container">
+      <fluent-breadcrumb>
+        <fluent-breadcrumb-item href="/">Home</fluent-breadcrumb-item>
+        <fluent-breadcrumb-item href="#">Users</fluent-breadcrumb-item>
+      </fluent-breadcrumb>
     </div>
 
-    <div>
-      <input type="text" class="search" placeholder="Search..." />
-      <fluent-button appearance="accent">Add filter</fluent-button>
+    <div class="title-container">
+      <h2>{{ pageTitle }}</h2>
     </div>
 
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
+    <div class="content-container">
+      <div class="toolbar-container">
+        <button>New user</button>
+        <button>Download users</button>
+        <button>Bulk operations</button>
+        <button>Refresh</button>
+        <button>Delete</button>
+      </div>
 
-      <tbody>
-        <tr v-for="user in users.items" :key="user.id">
-          <td>
-            <NuxtLink :to="`/users/${user.id}`">{{ user.name }}</NuxtLink>
-          </td>
-          <td>{{ user.email }}</td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="message-bar-container">
+        <p>Azure Active Directory is now Microsoft Entra ID.</p>
+      </div>
+
+      <div class="data-grid-toolbar-container">
+        <input type="text" placeholder="Search" />
+        <button>Add filter</button>
+      </div>
+
+      <div class="search-result-message-container">1 user found</div>
+
+      <div class="data-grid-container">
+        <table>
+          <thead>
+            <tr>
+              <th><input type="checkbox" /></th>
+              <th>UID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for="user in users.items" :key="user.id">
+              <td><input type="checkbox" /></td>
+              <td>{{ user.id }}</td>
+              <td>{{ user.name }}</td>
+              <td>{{ user.email }}</td>
+              <td>{{ user.createdAt }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-// Get options
+const pageTitle = "Users";
 const resourceServerOptions = useRuntimeConfig().public.resourceServer;
 
-// Get router
-const router = useRouter();
-
-// Fetch users
 const {
   data: users,
   error,
   execute,
   refresh,
 } = await useFetch(`${resourceServerOptions.baseAddress}/api/v1/users`);
-
-function navigateToCreate() {
-  console.log("Navigate to create user");
-}
 </script>
+
+<style>
+.breadcrumb-container {
+  background-color: yellowgreen;
+}
+
+.title-container {
+  background-color: lightblue;
+}
+
+.content-container {
+  background-color: lightcoral;
+}
+
+.toolbar-container {
+  background-color: lightcyan;
+}
+.message-bar-container {
+  background-color: lightgray;
+}
+.data-grid-toolbar-container {
+  background-color: lightgoldenrodyellow;
+}
+
+.search-result-message-container {
+  background-color: lightgreen;
+}
+.data-grid-container {
+  background-color: lightpink;
+}
+</style>
