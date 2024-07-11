b8b5e58749c2ac6fb69f0a471489bf36d1b5045a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-12 01:35:35 +0800

    create signal source

--- src/pages/signal-sources/create.vue
+++ src/pages/signal-sources/create.vue
@@ -1,7 +1,59 @@
 <template>
-  <div>Create new signal source</div>
+  <div>
+    <div>
+      <div>
+        <label>Id</label>
+      </div>
+      <input v-model="request.id" />
+    </div>
+
+    <div>
+      <div>
+        <label>Name</label>
+      </div>
+      <input v-model="request.name" />
+    </div>
+
+    <div>
+      <div>
+        <label>Description</label>
+      </div>
+      <input v-model="request.description" />
+    </div>
+
+    <div @click="createSignalSource" role="button"><span>Create</span></div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+type CreateSignalSourceRequest = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+const request = ref({
+  id: "",
+  name: "",
+  description: "",
+});
+
+const resourceServiceOptions = useRuntimeConfig().public.resourceService;
+
+async function createSignalSource() {
+  const response = await $fetch(
+    `${resourceServiceOptions.baseAddress}/api/v1/signal-sources`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: request.value,
+    }
+  );
+
+  return response;
+}
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/pages/signals/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Signal sources</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped></style>
