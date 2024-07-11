b6c574bdabba537c117bc93b17de0c207ae01527
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-12 04:21:53 +0800

    list signals

--- src/pages/signals/index.vue
+++ src/pages/signals/index.vue
@@ -1,7 +1,54 @@
 <template>
-  <div>Signal sources</div>
+  <div>
+    <MessageBar />
+
+    <div>
+      <input type="text" placeholder="Search" />
+    </div>
+
+    <div>
+      {{ signals.totalCount }}
+      {{ signals.totalCount === 1 ? "signal" : "signals" }}
+      found
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>
+            <input type="checkbox" />
+          </th>
+          <th>Time</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="signal in signals.items" :key="signal.id">
+          <td>
+            <input type="checkbox" />
+          </td>
+
+          <td>
+            <div>
+              {{ signal.time }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const resourceServiceOptions = useRuntimeConfig().public.resourceService;
+
+const {
+  data: signals,
+  error,
+  execute,
+  refresh,
+} = await useFetch(`${resourceServiceOptions.baseAddress}/api/v1/signals`);
+</script>
 
 <style lang="scss" scoped></style>
