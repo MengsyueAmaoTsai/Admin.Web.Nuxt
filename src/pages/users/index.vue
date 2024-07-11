22972199ea70d4e513358fcc782f5c8ae14f4b8f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-07-11 21:26:55 +0800

    list users in data grid

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,7 +1,125 @@
 <template>
-  <div></div>
+  <div>
+    <!-- Command Bar -->
+    <div>
+      <div>New user</div>
+      <div>Download users</div>
+      <div>Bulk operations</div>
+      <div>Refresh</div>
+      <div>Manage view</div>
+      <div>Delete</div>
+      <div>Per-user MFA</div>
+    </div>
+    <!-- Message Bar -->
+
+    <!-- Filter Bar -->
+
+    <!-- Result Message-->
+    <div>
+      {{ users.totalCount }}
+      {{ users.totalCount === 1 ? "user" : "users" }} found
+    </div>
+
+    <!-- Data grid -->
+    <table>
+      <thead>
+        <tr>
+          <input type="checkbox" />
+          <th>Name</th>
+          <th>Email</th>
+          <th>Created at</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="user in users.items" :key="user.id">
+          <input type="checkbox" />
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ user.createdAt }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const resourceServerOptions = useRuntimeConfig().public.resourceServer;
 
-<style scoped lang="scss"></style>
+const {
+  data: users,
+  error,
+  execute,
+  refresh,
+} = await useFetch(`${resourceServerOptions.baseAddress}/api/v1/users`);
+</script>
+
+<style scoped lang="scss">
+.command-bar__button-container {
+  flex-shrink: 0;
+  display: inherit;
+}
+
+.command-bar__button {
+  outline: transparent;
+  position: relative;
+  -webkit-font-smoothing: antialiased;
+  font-size: 13px;
+  font-weight: 400;
+  border: none;
+  border-radius: 0px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  padding: 0px 4px;
+  text-decoration: none;
+  text-align: center;
+  min-width: 40px;
+  height: 100%;
+  user-select: none;
+  font-family: inherit;
+}
+.text-container {
+  flex-grow: 1;
+  display: block;
+  font-family: inherit;
+  font-size: 13px;
+  font-weight: 400;
+  border: none;
+  border-radius: 0px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  padding: 0px 4px;
+  text-decoration: none;
+  text-align: center;
+  min-width: 40px;
+  height: 100%;
+  user-select: none;
+  outline: transparent;
+  position: relative;
+}
+
+.command-bar__button-text {
+  font-family: inherit;
+  outline: transparent;
+  position: relative;
+  font-size: 13px;
+  border: none;
+  border-radius: 0px;
+  box-sizing: border-box;
+  cursor: pointer;
+  display: inline-block;
+  padding: 0px 4px;
+  text-decoration: none;
+  text-align: center;
+  min-width: 40px;
+  height: 100%;
+  user-select: none;
+  margin: 0px 4px;
+  line-height: 100%;
+  display: block;
+  font-weight: normal;
+  white-space: nowrap;
+}
+</style>
