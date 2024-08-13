69e3c660b20ccc3db5569219d1ea9394ece8f451
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-14 04:40:19 +0800

    update list data feeds

--- src/pages/data-feeds/index.vue
+++ src/pages/data-feeds/index.vue
@@ -1,15 +1,37 @@
 <template>
   <div>
     <h1>Data Feeds</h1>
-    <ul>
-      <li v-for="dataFeed in dataFeeds" :key="dataFeed.dataFeedName">
-        <NuxtLink :to="`/data-feeds/${dataFeed.dataFeedName}`">
-          {{ dataFeed.dataFeedName }}
-        </NuxtLink>
-        -
-        {{ dataFeed.status }}
-      </li>
-    </ul>
+
+    <button>Start all</button>
+    <button>Stop all</button>
+    <button>Restart all</button>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Data Feed Name</th>
+          <th>Status</th>
+          <th>Started Time</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="dataFeed in dataFeeds" :key="dataFeed.dataFeedName">
+          <td>{{ dataFeed.dataFeedName }}</td>
+          <td>{{ dataFeed.status }}</td>
+          <td>
+            {{ dataFeed.status === "Running" ? dataFeed.startedTime : "--" }}
+          </td>
+          <td>
+            {{
+              dataFeed.status === "Running" && dataFeed.requestLatency
+                ? dataFeed.requestLatency
+                : 0
+            }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -21,6 +43,8 @@ const {
   execute,
   refresh,
 } = await useFetch(`${resourceServiceOptions.baseAddress}/api/v1/data-feeds`);
+
+console.log(dataFeeds);
 </script>
 
 <style lang="scss" scoped></style>
