69148f0e716b14071dae31257a40fff71b6a59d8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-15 03:42:16 +0800

    New user

--- src/pages/users/index.vue
+++ src/pages/users/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div>
+    <h1>Users</h1>
+
     <div>
-      <button @click="newUser">New user</button>
+      <button @click="newUserFormVisible = true">New user</button>
       <button @click="refreshData">Refresh</button>
-      <button>Manage View</button>
-      <button>Delete</button>
     </div>
 
-    <table>
+    <!-- New user form -->
+    <div v-if="newUserFormVisible">
+      <h2>New user</h2>
+
+      <form @submit.prevent="createUser">
+        <div>
+          <label for="name">Name</label>
+          <input type="text" v-model="createUserRequest.name" />
+        </div>
+
+        <div>
+          <label for="email">Email</label>
+          <input type="email" v-model="createUserRequest.email" />
+        </div>
+
+        <div>
+          <button type="submit">Create</button>
+          <button type="button" @click="newUserFormVisible = false">
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
+
+    <!-- User data -->
+    <div v-if="users.length === 0">No data</div>
+
+    <table v-else>
       <thead>
         <tr>
           <th>Id</th>
@@ -18,12 +45,8 @@
       </thead>
       <tbody>
         <tr v-for="user in users" :key="user.id">
-          <td>
-            <NuxtLink :to="`/users/${user.id}`">{{ user.id }}</NuxtLink>
-          </td>
-          <td>
-            <NuxtLink :to="`/users/${user.id}`">{{ user.name }}</NuxtLink>
-          </td>
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
           <td>{{ user.email }}</td>
           <td>{{ user.createdTime }}</td>
         </tr>
@@ -33,11 +56,8 @@
 </template>
 
 <script lang="ts" setup>
-definePageMeta({
-  title: "Users",
-  description: "List of users",
-  layout: "resources-list",
-});
+const newUserFormVisible = ref(false);
+const createUserRequest = ref({ name: "", email: "" });
 
 const resourceServiceOptions = useRuntimeConfig().public.resourceService;
 
@@ -48,14 +68,23 @@ const {
   refresh,
 } = await useFetch(`${resourceServiceOptions.baseAddress}/api/v1/users`);
 
-const router = useRouter();
+const createUser = async () => {
+  try {
+    await $fetch(`${resourceServiceOptions.baseAddress}/api/v1/users`, {
+      method: "POST",
+      body: createUserRequest.value,
+    });
 
-function newUser() {
-  router.push("/users/create");
-}
+    createUserRequest.value = { name: "", email: "" };
+    newUserFormVisible.value = false;
+
+    await refresh();
+  } catch (err) {
+    console.error("Error adding user:", err);
+  }
+};
+
 const refreshData = async () => {
   await refresh();
 };
 </script>
-
-<style lang="scss" scoped></style>
