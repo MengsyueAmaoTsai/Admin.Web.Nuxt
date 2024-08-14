2b611d7296318bcde9c91606a1ca946d2d60d55f
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-15 05:14:20 +0800

    Create signal source

--- src/pages/signal-sources/index.vue
+++ src/pages/signal-sources/index.vue
@@ -1,59 +1,62 @@
 <template>
   <div>
-    <div>
-      <button @click="newSignalSource">New signal source</button>
-      <button>Refresh</button>
-      <button>Manage View</button>
-      <button>Delete</button>
-    </div>
-
-    <MessageBar />
-    <SearchBox />
+    <h1>Signal Sources</h1>
 
     <div>
-      {{ signalSources.totalCount }}
-      {{ signalSources.totalCount === 1 ? "signal source" : "signal sources" }}
-      found
+      <button @click="newSignalSourceFormVisible = true">
+        New signal source
+      </button>
+      <button @click="refreshData">Refresh</button>
+    </div>
+    <!-- New user form -->
+    <div v-if="newSignalSourceFormVisible">
+      <h2>New signal source</h2>
+
+      <form @submit.prevent="createSignalSource">
+        <div>
+          <label for="name">Id</label>
+          <input type="text" v-model="createSignalSourceRequest.id" />
+        </div>
+
+        <div>
+          <label for="name">Name</label>
+          <input type="text" v-model="createSignalSourceRequest.name" />
+        </div>
+
+        <div>
+          <label for="description">Description</label>
+          <input type="text" v-model="createSignalSourceRequest.description" />
+        </div>
+
+        <div>
+          <button type="submit">Submit</button>
+          <button type="button" @click="newSignalSourceFormVisible = false">
+            Cancel
+          </button>
+        </div>
+      </form>
     </div>
 
-    <table>
+    <div v-if="signalSources.length === 0">No data</div>
+
+    <table v-else>
       <thead>
         <tr>
-          <th>
-            <input type="checkbox" />
-          </th>
           <th>Id</th>
           <th>Name</th>
           <th>Description</th>
-          <th>Created at</th>
+          <th>Published</th>
+          <th>Created time</th>
         </tr>
       </thead>
 
       <tbody>
-        <tr v-for="signalSource in signalSources.items" :key="signalSource.id">
-          <td>
-            <input type="checkbox" />
-          </td>
-
-          <td>
-            <NuxtLink :to="`/signal-sources/${signalSource.id}`">
-              <div>
-                {{ signalSource.id }}
-              </div>
-            </NuxtLink>
-          </td>
-
-          <td>
-            <NuxtLink :to="`/signal-sources/${signalSource.id}`">
-              <div>
-                {{ signalSource.name }}
-              </div>
-            </NuxtLink>
-          </td>
-
+        <tr v-for="signalSource in signalSources" :key="signalSource.id">
+          <td>{{ signalSource.id }}</td>
+          <td>{{ signalSource.name }}</td>
           <td>{{ signalSource.description }}</td>
-
-          <td>{{ signalSource.createdAt }}</td>
+          <td>{{ signalSource.isPublished }}</td>
+          <td>{{ signalSource.createdTime }}</td>
         </tr>
       </tbody>
     </table>
@@ -62,6 +65,8 @@
 
 <script lang="ts" setup>
 const resourceServiceOptions = useRuntimeConfig().public.resourceService;
+const newSignalSourceFormVisible = ref(false);
+const createSignalSourceRequest = ref({ id: "", name: "", description: "" });
 
 const {
   data: signalSources,
@@ -72,11 +77,28 @@ const {
   `${resourceServiceOptions.baseAddress}/api/v1/signal-sources`
 );
 
-const router = useRouter();
+const createSignalSource = async () => {
+  try {
+    await $fetch(
+      `${resourceServiceOptions.baseAddress}/api/v1/signal-sources`,
+      {
+        method: "POST",
+        body: createSignalSourceRequest.value,
+      }
+    );
 
-function newSignalSource() {
-  router.push("/signal-sources/create");
-}
+    createSignalSourceRequest.value = { id: "", name: "", description: "" };
+    newSignalSourceFormVisible.value = false;
+
+    await refresh();
+  } catch (err) {
+    console.error("Error adding user:", err);
+  }
+};
+
+const refreshData = async () => {
+  await refresh();
+};
 </script>
 
 <style lang="scss" scoped></style>
