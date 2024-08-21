bc85972db0cd70ed69e767e2206fb3cf45342dfa
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-21 10:20:05 +0800

    Add current user

--- src/components/TopBar/CurrentUser.vue
+++ src/components/TopBar/CurrentUser.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>
-    <div>{{ currentUser.email }}</div>
-    <div>{{ currentUser.name }}</div>
+  <div class="current-user">
+    <button class="current-user-menu-button">
+      <div class="current-user-menu-header">
+        <div class="current-user-info-container">
+          <div class="current-user-email">{{ currentUser.email }}</div>
+          <div class="current-user-name">{{ currentUser.name }}</div>
+        </div>
+
+        <div class="current-user-avatar-container">
+          <img class="current-user-avatar-image" :src="currentUser.avatar" />
+        </div>
+      </div>
+    </button>
   </div>
 </template>
 
@@ -9,7 +19,78 @@
 const currentUser = {
   name: "Mengsyue Amao Tsai",
   email: "mengsyue.tsai@outlook.com",
+  avatar: "https://avatars.githubusercontent.com/u/88216325",
 };
 </script>
 
-<style></style>
+<style>
+.current-user {
+  position: relative;
+  flex: 0 0 auto;
+  box-sizing: border-box;
+  z-index: 200;
+}
+
+.current-user-menu-button {
+  border: 0;
+  padding: 0;
+  margin: 0;
+  background: none;
+  cursor: pointer;
+  display: flex;
+  position: relative;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  fill: #fff;
+}
+
+.current-user-menu-header {
+  position: relative;
+  display: flex;
+  align-items: center;
+  height: 40px;
+}
+
+.current-user-info-container {
+  text-align: left;
+  flex: 1 1 auto;
+  padding-left: 20px;
+}
+
+.current-user-email {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-size: 14px;
+  line-height: normal;
+  max-width: 160px;
+}
+
+.current-user-name {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  font-weight: 600;
+  font-size: 10px;
+  line-height: normal;
+  text-transform: uppercase;
+  max-width: 160px;
+}
+
+.current-user-avatar-container {
+  height: 28px;
+  width: 28px;
+  flex: 0 0 auto;
+  padding-left: 6px;
+  padding-right: 6px;
+  box-sizing: content-box;
+}
+
+.current-user-avatar-image {
+  height: 28px;
+  width: 28px;
+  border: 0;
+  border-radius: 28px;
+}
+</style>
