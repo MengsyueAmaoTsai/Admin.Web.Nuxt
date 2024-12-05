4118a3eb6c8f3499f2ef15f58689674de2fae08b
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-06 07:03:48 +0800

    Refactor CRUD

--- src/pages/users/new.vue
+++ src/pages/users/new.vue
@@ -1,3 +1,50 @@
 <template>
-  <div></div>
+  <div>
+    <div class="row">
+      <label>Email</label>
+      <input type="email" v-model="email" />
+    </div>
+    <div class="row">
+      <label>Name</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div class="row">
+      <label>Password</label>
+      <input type="password" v-model="password" />
+    </div>
+
+    <div class="row">
+      <Button @click="createUser">Create</Button>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { $resourceService } = useNuxtApp();
+
+const email = ref("");
+const name = ref("");
+const password = ref("");
+
+const createUser = async () => {
+  try {
+    const createdResponse = await $resourceService.createUser({
+      email: email.value,
+      name: name.value,
+      password: password.value,
+    });
+
+    alert(`User ${createdResponse.id} created`);
+    navigateTo("/users");
+  } catch (error) {
+    alert(`Failed to create user: ${error}`);
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.row {
+  display: flex;
+  margin-bottom: 1rem;
+}
+</style>
