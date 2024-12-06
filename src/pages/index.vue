6518fb148ad7e0900abcc3c9d351a53176e656ce
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-06 15:09:50 +0800

    update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Welcome to admin-web web!</h1>
+  <h2>RichillCapital services</h2>
+  <h2>Resources</h2>
+  <h2>Navigate</h2>
+  <h2>Tools</h2>
+  <h2>Useful links</h2>
 </template>
