eadd1800078b5a47be5f3b6fc28f54584f47310a
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-20 03:12:01 +0800

    Refactor signal-sources/new.vue

--- src/pages/signal-sources/new.vue
+++ src/pages/signal-sources/new.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <h2>Basic</h2>
+
     <div class="row">
       <label>Id</label>
       <fluent-text-field
@@ -24,12 +26,18 @@
 
     <div class="row">
       <label>Version</label>
-      <input type="text" v-model="version" />
+      <input type="number" v-model="versionMajor" />
+      <input type="number" v-model="versionMinor" />
+      <input type="number" v-model="versionPatch" />
     </div>
 
     <div class="row">
       <label>Stage</label>
-      <input type="text" v-model="stage" />
+      <select v-model="stage" required>
+        <option v-for="option in stageOptions" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
     </div>
 
     <div class="row">
@@ -46,13 +54,33 @@
 <script setup lang="ts">
 import type { ISignalSourceService } from "~/resources/signal-sources";
 
+enum StageOptions {
+  Development = "Development",
+  BackTesting = "BackTesting",
+  Simulation = "Simulation",
+  Production = "Production",
+  Paused = "Paused",
+  Deprecated = "Deprecated",
+}
+
+const stageOptions = [
+  { label: "Development", value: StageOptions.Development },
+  { label: "Back testing", value: StageOptions.BackTesting },
+  { label: "Simulation", value: StageOptions.Simulation },
+  { label: "Production", value: StageOptions.Production },
+  { label: "Paused", value: StageOptions.Paused },
+  { label: "Deprecated", value: StageOptions.Deprecated },
+];
+
 const signalSourceService = useNuxtApp()
   .$signalSourceService as ISignalSourceService;
 
 const id = ref("");
 const name = ref("");
 const description = ref("");
-const version = ref("");
+const versionMajor = ref(0);
+const versionMinor = ref(0);
+const versionPatch = ref(1);
 const stage = ref("");
 
 const createSignalSource = async () => {
@@ -61,7 +89,7 @@ const createSignalSource = async () => {
       id: id.value,
       name: name.value,
       description: description.value,
-      version: version.value,
+      version: `${versionMajor.value}.${versionMinor.value}.${versionPatch.value}`,
       stage: stage.value,
     });
 
@@ -71,6 +99,10 @@ const createSignalSource = async () => {
     alert(`Failed to create signal source: ${error}`);
   }
 };
+
+onMounted(() => {
+  stage.value = StageOptions.Development;
+});
 </script>
 
 <style scoped lang="scss">
