0d5a72fe058a0ddc19726916815b52bc90182af1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-12-21 10:03:22 +0800

    add /demo/progress-ring

---
+++ src/pages/demo/progress-ring.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>Default</h4>
+    <div
+      style="
+        width: 300px;
+        display: grid;
+        grid-gap: 12px;
+        grid-auto-flow: column;
+      "
+    >
+      <ProgressRing :min="0" :max="100" v-model="fixedValue"></ProgressRing>
+      <ProgressRing></ProgressRing>
+    </div>
+
+    <h4>Stroke and Color</h4>
+
+    <h4>Paused</h4>
+  </div>
+</template>
+
+<script setup lang="ts">
+const fixedValue = 75;
+</script>
+
+<style></style>
